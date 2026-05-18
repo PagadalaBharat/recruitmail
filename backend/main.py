1744f9b400da31a09825f018fe895e785f7d305f
Bharat Pagadala Office <122111256+PagadalaBharat@users.noreply.github.com>
2026-05-18 10:56:19 +0530

    Open CORS to debug

--- backend/main.py
+++ backend/main.py
@@ -18,8 +18,8 @@ if FRONTEND_URL:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
